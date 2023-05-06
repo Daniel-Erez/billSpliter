937eec7ca2fc3d58fd36657b27b8f7e3bc1c387c
Daniel280200 <danieler@post.jce.ac.il>
2023-05-06 20:33:41 +0300

    selectPayers

--- src/components/SelectPayers.jsx
+++ src/components/SelectPayers.jsx
@@ -1,14 +1,18 @@
 import Dropdown from 'react-bootstrap/Dropdown';
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import Form from 'react-bootstrap/Form';
 import { useFriendsState } from '../hooks/FriendsContext';
 
+const MAX_FRIENDS_SELECT_NUMBER=5
+
 function SelectPayers() {
     const [friendsList,] = useFriendsState()
+    const [selectedFriends,settt]=useState([])
+
     return (
         <Dropdown style={{direction:"ltr"}}>
             <Dropdown.Toggle variant="light" id="dropdown-basic">
-                Dropdown Button
+                {selectedFriends.length===0? "יש לבחור משלמים" :selectedFriends.length>MAX_FRIENDS_SELECT_NUMBER?`${MAX_FRIENDS_SELECT_NUMBER+1}+`:selectedFriends.toString()}
             </Dropdown.Toggle>
             <Dropdown.Menu>
     {friendsList.map(friend=>
@@ -17,6 +21,13 @@ function SelectPayers() {
                 key={friend}
                 name={friend}
                 type="checkbox"
+                onChange={(e)=>{
+                    if (e.target.checked){
+                        settt(pre=>[...pre, e.target.name])
+                    }else{
+                        settt(pre=>pre.reduce(((s,n)=> (n!==e.target.name)? [...s, n]:s),[]))
+                    }
+                }}
                 label={friend} />
                 )}
             </Dropdown.Menu>
